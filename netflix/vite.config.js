d908d0e233e102e08c6153e5fa76a850cd5af4c0
KAKA12-19 <evenezertadesse@gmail.com>
2025-06-03 21:46:50 +0300

    last change

--- netflix/vite.config.js
+++ netflix/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://evenezer-tadesse.github.io/Netflix-clone",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    manifest: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[hash][extname]",
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
+  },
+});
